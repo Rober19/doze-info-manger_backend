3fdb9afb75b4ab552ec1974558839e41393cd872
rober19 <texas_telecaster@hotmail.com>
2019-03-30 11:31:18 -0500

    [rober19] [03_30_19] auto-deploy

--- deploy.bat
+++ deploy.bat
@@ -25,5 +25,5 @@ set datestr=%month%_%day%_%year%
 REM tambien en la fecha se puede usar el %date%
 git commit -m "[%gituser%] [%datestr%] %commit% "
 git push origin master
-git push heroku master
+::git push heroku master
 pause
